d6b985bf57911880446a169c56294714d6aadf46
Victor Valerio <victor.dmv@unifei.edu.br>
2021-04-12 22:10:05 -0300

    falta ajustar os tamanhos das fontes e adicionar figura.

--- R/base_analise.R
+++ R/base_analise.R
@@ -89,26 +89,62 @@ labels$angle <- ifelse(angle < -90, (angle + 180), angle)
 
 
 
-rm(temp_sep)
+# legend_plot1 <- data.frame(
+#
+#   type = c('Less Efficient', 'Target Efficiency'),
+#
+#   value = c('1', '1'))
+
+
+
+
+rm(data, temp_sep, number_of_bar, angle)
 
 
 
 
 # Visualização -------------------------------------------------------------------------------------
+plot1 <- pensions %>%
 
-pensions %>%
-
-  ggplot2::ggplot(mapping = ggplot2::aes(x = as.factor(id), y = efficiency, fill = -efficiency)) +
+  ggplot2::ggplot(mapping = ggplot2::aes(x = as.factor(id), y = efficiency, fill = efficiency)) +
 
   ggplot2::geom_bar(stat = 'identity') +
+
+  ggplot2::geom_text(data = labels,
+
+                     mapping = ggplot2::aes(x = id, y = efficiency + 3, label = country, hjust = hjust),
+
+                     color = 'white', alpha = 1, size = 4, family = 'oswald', fontface = 'bold',
+
+                     angle = labels$angle, inherit.aes = FALSE, na.rm = TRUE) +
+
+  ggplot2::coord_cartesian(clip = 'on') +
 
   ggplot2::ylim(-120,120) +
 
   ggplot2::coord_polar() +
 
+  ggplot2::scale_fill_gradient(low = '#F25D27', high = '#03A696') +
+
   ggplot2::theme_minimal() +
 
+  ggplot2::labs(title = 'Efficiency Scores',
+
+                fill = 'Target \nEfficiency') +
+
   ggplot2::theme(
+
+    title = ggplot2::element_text(color = 'white', size = 30,
+
+                                  face = 'bold', family = 'oswald'),
+
+    legend.position = 'left',
+
+    legend.title = ggplot2::element_text(color = 'white', size = 15,
+
+                                         face = 'bold', family = 'oswald'),
+
+    legend.text = ggplot2::element_text(color = 'grey20'),
 
     axis.text = ggplot2::element_blank(),
 
@@ -116,15 +152,105 @@ pensions %>%
 
     panel.grid = ggplot2::element_blank(),
 
-    plot.margin = ggplot2::unit(rep(-1,4), "cm")) +
+    panel.background = ggplot2::element_rect(fill = 'grey20', color = NA),
 
-  ggplot2::geom_text(data = labels,
+    plot.margin = ggplot2::unit(rep(-1,4), 'cm'))
 
-            mapping = ggplot2::aes(x = id, y = efficiency + 3, label = country, hjust = hjust),
 
-            color = 'black', fontface = 'bold', alpha = 1, size = 4,
 
-            angle = labels$angle, inherit.aes = FALSE, na.rm = TRUE)
+
+
+
+plot2 <- pensions %>%
+
+  ggplot2::ggplot() +
+
+  ggplot2::geom_point(mapping = ggplot2::aes(x = pop_65_percent, y = gov_spend_percent_gdp,
+
+                                             color = efficiency), size = 3) +
+
+  ggplot2::scale_color_gradient(low = '#F25D27', high = '#03A696') +
+
+  ggplot2::labs(title = 'Base Data',
+
+                x = 'Population aged 65 years and over, % of total',
+
+                y = 'Government speding on pension benefits % of GDP') +
+
+  ggplot2::xlim(0,30) +
+
+  ggplot2::ylim(0,20) +
+
+  ggplot2::theme_minimal() +
+
+  ggplot2::theme(
+
+    title = ggplot2::element_text(color = 'white', size = 30,
+
+                                  face = 'bold', family = 'oswald'),
+
+    legend.position = 'none',
+
+    axis.text = ggplot2::element_text(color = 'white', size = 15, face = 'bold', family = 'oswald'),
+
+    axis.title = ggplot2::element_text(color = 'white', size = 20, face = 'bold', family = 'oswald'),
+
+    panel.background = ggplot2::element_rect(fill = 'grey20', color = NA))
+
+plot2
+
+
+final <- plot2 / plot1 +
+
+  patchwork::plot_layout(ncol = 2) +
+
+  patchwork::plot_annotation(
+
+    title = "Brazil's Golden Oldie Blowout",
+
+    subtitle = 'Latest Avaiable',
+
+    caption =
+
+      'Author: Victor Valerio (@victor_dmv) | Data: The Economist',
+
+    theme = ggplot2::theme(
+
+      plot.background = ggplot2::element_rect(fill = 'grey20', color = NA),
+
+      plot.title = ggplot2::element_text(family = 'oswald', size = 30, color = 'white',
+
+                                         hjust = 0.5, face = 'bold'),
+
+      plot.subtitle = ggplot2::element_text(family = 'oswald', color = 'white', size = 30,
+
+                                            hjust = 0.5, margin = ggplot2::margin(10,0,15,0)),
+
+      plot.caption = ggplot2::element_text(family = 'techmono', color = 'grey80', size = 15,
+
+                                           hjust = 0.98)))
+
+
+final
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
